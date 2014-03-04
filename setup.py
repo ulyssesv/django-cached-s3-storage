8c7815d203b7aa7c3b14e1db586df9e0e88e5e24
Alan Justino <alan.justino@yahoo.com.br>
2014-03-04 19:05:33 -0300

    Using uncompress gzipped files before using locally

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     name='django-cached-s3-storage',
     version='0.2.1',
     py_modules=['cached_s3_storage'],
+    install_requires = ['python-magic'],
     license='MIT',
     author='Ulysses V',
     author_email='uvilela [at] inoa.com.br',
